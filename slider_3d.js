2a931df218bb47d5e9f4242c401c55eece3169a2
Fen1xx <provkovasu15@gmail.com>
2022-05-02 03:09:50 +0500

    Refactoring: add a class "Slider3D"

--- slider_3d.js
+++ slider_3d.js
@@ -1,43 +1,67 @@
-const perspectiveValue = -1000;
-const zValues = [];
-const frames = document.querySelectorAll('.frame');
-const timePause = 700;
+class Slider3D {
+    perspectiveValue = -1000;
+    zValues = [];
+    frames = document.querySelectorAll('.frame');
+    timePause = 700;
+    stepZValue = 10;
 
-frames.forEach( (item, index) => {
-    let zValue = index * perspectiveValue;
-    zValues.push(zValue);
-    item.style.transform = `translateZ(${zValue}px)`;
-})
-
-scroll3d = pauseDecorator(scroll3d, timePause);
-
-document.addEventListener('wheel', scroll3d);
-
-function scroll3d(e) {
-    if ((zValues[0] <= 0 && e.deltaY < 0) ||
-         (zValues[zValues.length - 1] >= 0 && e.deltaY > 0)) {
-        return;
+    setBeginZValues() {
+        this.frames.forEach( (item, index) => {
+            let zValue = index * this.perspectiveValue;
+            this.zValues.push(zValue);
+            item.style.transform = `translateZ(${zValue}px)`;
+        });
     }
 
-    frames.forEach( (item, index) => {
-        zValues[index] += e.deltaY * 10;
-        item.style.transform = `translateZ(${zValues[index]}px)`;
-        if (zValues[index] > 0) {
+    setOpacitySlides(item, index) {
+        if (this.zValues[index] > 0) {
             item.style.opacity = '0';
         } else {
             item.style.opacity = '1';
         }
-        
-    }); 
+    }
+
+    isEndOfSlidesRange(e) {
+        if ((this.zValues[0] <= 0 && e.deltaY < 0) ||
+            (this.zValues[this.zValues.length - 1] >= 0 && e.deltaY > 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    changeZValue(item, index, e) {
+        this.zValues[index] += e.deltaY * this.stepZValue;
+        item.style.transform = `translateZ(${this.zValues[index]}px)`;
+    }
+
+    scroll(e) {
+        if (this.isEndOfSlidesRange(e)) return;
+
+        this.frames.forEach( (item, index) => {
+            this.changeZValue(item, index, e);
+            this.setOpacitySlides(item, index);
+        }); 
+    }
+
+    pauseScrollDecorator(func, ms) {
+        let isCooldown = false;
+        return (e) => {
+            if (isCooldown) return;
+            
+            func.call(this, e);
+            isCooldown = true;
+            setTimeout( () => isCooldown = false, ms);
+        };
+    }
+
+    init() {
+        this.setBeginZValues();
+
+        this.scroll = this.pauseScrollDecorator(this.scroll, this.timePause);
+
+        document.addEventListener('wheel', this.scroll);
+    }
 }
 
-function pauseDecorator(func, ms) {
-    let isCooldown = false;
-    return (e) => {
-        if (isCooldown) return;
-
-        func(e);
-        isCooldown = true;
-        setTimeout( () => isCooldown = false, ms);
-    };
-}
+const slider3d = new Slider3D();
+slider3d.init();
