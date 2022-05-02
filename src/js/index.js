a1e946807345787d4ace485c4e559bcbe10b0cf3
Fen1xx <provkovasu15@gmail.com>
2022-05-03 01:31:18 +0500

    Initialization of slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 import '../styles/style.scss';
+import '../modules/slider_3d/slider_3d.scss';
+import Slider3D from '../modules/slider_3d/slider_3d.js';
+
+const slider3d = new Slider3D();
+slider3d.init();
