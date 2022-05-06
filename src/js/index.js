ed2abb418187fcd22d91d4f68fd3e3f5d0ea22bf
Fen1xx <provkovasu15@gmail.com>
2022-05-06 21:18:37 +0500

    Add a common styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import '../styles/style.scss';
 import '../modules/slider_3d/slider_3d.scss';
+import '../styles/style.scss';
 import Slider3D from '../modules/slider_3d/slider_3d.js';
 
 const slider3d = new Slider3D();
