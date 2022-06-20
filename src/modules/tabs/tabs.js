52220a0803d03a0c73c0228a753d6443487b8780
Fen1xx <provkovasu15@gmail.com>
2022-06-21 02:25:40 +0500

    Tabs: change the percentage calculation method for moving an element

--- src/modules/tabs/tabs.js
+++ src/modules/tabs/tabs.js
@@ -1,58 +1,10 @@
 export default class Tabs {
     tabsItems = document.querySelectorAll('.tabs__item');
     contents = document.querySelectorAll('.tabs__content');
-    countBorder = 2;
     firstTab = this.tabsItems[0];
-    beginWidthPercentTab = 100;
-    beginHeightPercentTab = 100;
-
-    setBeginWidthTab(item) {
-        this.beginWidthTab = parseFloat(getComputedStyle(item).width) + parseFloat(getComputedStyle(item).borderWidth) * this.countBorder;
-    }
-
-    getBeginWidthTab() {
-        return this.beginWidthTab;
-    }
-
-    setSelectedWidthTab(item) {
-        this.selectedWidthTab = parseFloat(item.getBoundingClientRect().width);
-    }
-
-    getSelectedWidthTab() {
-        return this.selectedWidthTab;
-    }
-
-    setPercentWidthTab() {
-        this.percentWidthTab = Math.round(this.getSelectedWidthTab() / this.getBeginWidthTab() * 100) - this.beginWidthPercentTab;
-    }
-
-    getPercentWidthTab() {
-        return this.percentWidthTab;
-    }
-
-    setBeginHeightTab(item) {
-        this.beginHeightTab = parseFloat(getComputedStyle(item).height) + parseFloat(getComputedStyle(item).borderWidth) * this.countBorder;
-    }
-
-    getBeginHeightTab() {
-        return this.beginHeightTab;
-    }
-
-    setSelectedHeightTab(item) {
-        this.selectedHeightTab = parseFloat(item.getBoundingClientRect().height) - parseFloat(getComputedStyle(item).borderWidth);
-    }
-
-    getSelectedHeightTab() {
-        return this.selectedHeightTab;
-    }
-
-    setPercentHeightTab() {
-        this.percentHeightTab = Math.round(this.getSelectedHeightTab() / this.getBeginHeightTab() * 100) - this.beginHeightPercentTab;
-    }
-
-    getPercentHeightTab() {
-        return this.percentHeightTab;
-    }
+    tabScale = 1.2;
+    tabScalePercent = (this.tabScale - 1) * 100;
+    countOppositeSidesOfElement = 2;
 
     hideContent(index) {
         for (let i = 0; i < this.contents.length; i++) {
@@ -85,24 +37,17 @@ export default class Tabs {
         item.style.borderColor = 'rgb(83, 32, 97)';
         item.style.borderTopLeftRadius = '4px';
         item.style.borderTopRightRadius = '4px';
-        item.style.transform = `scale(1.2)`;
         item.style.zIndex = '1';
         item.setAttribute('active', 'active');
 
-        this.setSelectedHeightTab(item);
-        this.setSelectedWidthTab(item);
-        this.setPercentHeightTab(item);
-        this.setPercentWidthTab(item);
-
         if (this.isOneTab()) {
-            item.style.transform = `scale(1)`;
-            item.style.height = `${parseInt(getComputedStyle(item).height) * 1.2}px`;
+            item.style.height = `${parseInt(item.offsetHeight) * this.tabScale}px`;
         } else if (this.isFirstActiveTab(item, index)) {
-            item.style.transform = `translate(${this.getPercentWidthTab() / 2}%, -${this.getPercentHeightTab() / 2}%) scale(1.2)`;
+            item.style.transform = `translate(${this.tabScalePercent / this.countOppositeSidesOfElement}%, -${this.tabScalePercent / this.countOppositeSidesOfElement}%) scale(${this.tabScale})`;
         } else if (this.isLastActiveTab(item, index)) {
-            item.style.transform = `translate(-${this.getPercentWidthTab() / 2}%, -${this.getPercentHeightTab() / 2}%) scale(1.2)`;
+            item.style.transform = `translate(-${this.tabScalePercent / this.countOppositeSidesOfElement}%, -${this.tabScalePercent / this.countOppositeSidesOfElement}%) scale(${this.tabScale})`;
         } else {
-            item.style.transform = `translateY(-${this.getPercentHeightTab() / 2}%) scale(1.2)`;
+            item.style.transform = `translateY(-${this.tabScalePercent / this.countOppositeSidesOfElement}%) scale(${this.tabScale})`;
         }
     }
 
@@ -114,9 +59,6 @@ export default class Tabs {
             item.style.zIndex = '0';
             item.removeAttribute('active');
         });
-
-        this.setBeginHeightTab(tab);
-        this.setBeginWidthTab(tab);
     }
 
     changeTab() {
@@ -131,14 +73,12 @@ export default class Tabs {
     }
 
     selectTabFirst(tab, index) {
-        this.setBeginStyleTabs(tab);
         this.setSelectedStyleTab(tab, index);
         this.showContent(index);
     }
 
     init() {
         this.selectTabFirst(this.firstTab, 0);
-
         this.changeTab();
     }
 }
